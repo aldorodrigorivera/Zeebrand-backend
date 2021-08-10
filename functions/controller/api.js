2e22fff4fde0a2a67281e57ab4f56355443a4f04
aldorodrigo <aldorodrigo.rivera@gmail.com>
2021-08-10 15:24:46 -0500

    WIP Adding CRUD

--- functions/controller/api.js
+++ functions/controller/api.js
@@ -11,7 +11,29 @@ module.exports = {
       const user = await query.get(id);
       res.status(200).send(user);
     } catch (error) {
-      res.status(500).send(error);
+      res.status(500).send(error.message);
+    }
+  },
+
+  getAllProducts: async (req, res) => {
+    try {
+      const query = new Parse.Query(BD.ProductClass);
+      query.addAscending("createdAt");
+      const products = await query.find();
+      res.status(200).send(products);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  },
+
+  insertProduct: async (req, res) => {
+    try {
+      const query = new Parse.Query(BD.ProductClass);
+      query.addAscending("createdAt");
+      const products = await query.find();
+      res.status(200).send(products);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
   },
 };
--- functions/routes/api.js
+++ functions/routes/api.js
@@ -5,5 +5,10 @@ const controller = require("../controller/api");
 const guard = require("../middleware/auth");
 
 router.get("/me", [guard.isAuth], controller.isMe);
+router.get("/products", [guard.isAuth], controller.getAllProducts);
+router.post("/products", [guard.isAuth], controller.insertProduct);
+router.get("/products/:id", [guard.isAuth], controller.getProduct);
+router.update("/products/:id", [guard.isAuth], controller.updateProduct);
+router.delete("/products/:id/delete", [guard.isAuth], controller.deleteProduct);
 
 module.exports = router;
