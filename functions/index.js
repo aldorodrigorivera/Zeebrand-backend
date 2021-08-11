4bd5e47beb4b6eedcc0a6818d6597b69d032e5f6
aldorodrigo <aldorodrigo.rivera@gmail.com>
2021-08-11 14:13:30 -0500

    Add cors

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 // Modules
-
 const api = require("./routes/api");
 const auth = require("./routes/auth");
+const options = {origin: "*"};
+app.use(cors(options));
 
 // Routes
 app.use("/api", api);
