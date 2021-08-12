c4a6a180ef3c5277047f2ce79d05569c74bfe67f
aldorodrigo <aldorodrigo.rivera@gmail.com>
2021-08-12 12:36:04 -0500

    Fix auth error

--- functions/middleware/auth.js
+++ functions/middleware/auth.js
@@ -5,27 +5,27 @@ const Parse = BD.initializeParse();
 module.exports = {
   isAuth: async (req, res, next) => {
     if (!req.headers["authorization"]) {
-      return res.status(401).send("No token provider");
+      return res.status(401).send({error: "No token provider"});
     }
     if (req.headers["authorization"].split(" ")[0] !== "Bearer") {
-      return res.status(401).send("Wrong type of token provider");
+      return res.status(401).send({error: "Wrong type of token provider"});
     }
 
     const token = req.headers["authorization"].split(" ")[1];
     try {
       const {id} = await jwt.validate(token);
       if (!id) {
-        return res.status(401).send("Invalid token");
+        return res.status(401).send({error: "Invalid token"});
       }
       const query = new Parse.Query(BD.UserClass);
       const user = await query.get(id);
       if (!user) {
-        return res.status(401).send("User not found, invalid token");
+        return res.status(401).send({error: "User not found, invalid token"});
       }
       req.userId = id;
       next();
     } catch (error) {
-      return res.status(500).send(error.message);
+      return res.status(401).send({error: error.message});
     }
   },
 };
