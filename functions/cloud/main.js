cf95cf956de67790811f12071da54cf984f5c29a
aldorodrigo <aldorodrigo.rivera@gmail.com>
2021-08-14 00:20:59 -0500

    Fix insert

--- functions/cloud/main.js
+++ functions/cloud/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-undef */
 // This cloud function is located in a parse-server instance
 // I added this code here to keep it in Github
@@ -9,20 +10,23 @@ const client = require("twilio")(twilioSID, twilioTOKEN);
 
 Parse.Cloud.beforeSave(("Product"), async (request) => {
   const query = new Parse.Query("Product");
-  const product = await query.get(request.object.id);
-  // Just in case the product price is changed
-  if (product.get("price") !== request.object.get("price")) {
-    const query = new Parse.Query("User");
-    query.equalTo("rol", "Admin");
-    const users = await query.find();
-    // All admins user will receive messages notification
-    const phones = users.map((user) => user.get("phone"));
-    client.messages.create({
-      body: `El producto con SKU: ${product.id} (${request.object.get("name")}) 
-      ha sido actualizado a: $${request.object.get("price")}MXN`,
-      from: twilioFROM,
-      to: phones,
-    }).then((message) => console.log("Mensaje enviados a:", phones))
-        .catch((err) => console.error(err));
+  query.equalTo("objectId", request.object.id);
+  const products = await query.find();
+  if (products.length > 0) {
+    const product = products[0];
+    // Just in case the product price is changed
+    if (product.get("price") !== request.object.get("price")) {
+      const query = new Parse.Query("User");
+      query.equalTo("rol", "Admin");
+      const users = await query.find();
+      // All admins user will receive messages notification
+      const phones = users.map((user) => user.get("phone"));
+      client.messages.create({
+        body: `El producto con SKU:${product.id} (${request.object.get("name")}) ha sido actualizado a: $${request.object.get("price")}MXN`,
+        from: twilioFROM,
+        to: phones,
+      }).then((message) => console.log("Mensaje enviados a:", phones))
+          .catch((err) => console.error(err));
+    }
   }
 });
